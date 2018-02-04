5a3b542ee5cbf52e647266a4766754b510faecae
Matheus Braga <matheusssbraga@gmail.com>
2018-02-04 17:20:50 -0300

    Criando um endpoint para criar uma nova categoria e outro para buscar uma categoria por ID

--- src/main/java/com/algaworks/algamoney/api/resource/CategoriaResource.java
+++ src/main/java/com/algaworks/algamoney/api/resource/CategoriaResource.java
@@ -1,11 +1,19 @@
 package com.algaworks.algamoney.api.resource;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.algaworks.algamoney.api.model.Categoria;
 import com.algaworks.algamoney.api.repository.CategoriaRepository;
@@ -20,6 +28,23 @@ public class CategoriaResource {
 	@GetMapping
 	public List<Categoria> listar() {
 		return categoriaRepository.findAll();
+	}
+	
+	@PostMapping
+	public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response) {
+		categoria = categoriaRepository.save(categoria);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
+				.buildAndExpand(categoria.getId()).toUri();
+		
+		response.setHeader("Location", uri.toASCIIString());
+	
+		return ResponseEntity.created(uri).body(categoria);
+	}
+	
+	@GetMapping("/{id}")
+	public Categoria buscarPorID(@PathVariable Long id) {
+		return categoriaRepository.findOne(id);
 	}
 	
 }
