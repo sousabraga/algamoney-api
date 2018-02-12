108a9872c7359a82d0e22d6e570754588b45b89e
Matheus Braga <matheusssbraga@gmail.com>
2018-02-12 17:23:30 -0300

    Refatorando para setar o header Location através de evento

---
+++ src/main/java/com/algaworks/algamoney/api/event/RecursoCriadoEvent.java
@@ -0,0 +1,29 @@
+package com.algaworks.algamoney.api.event;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.context.ApplicationEvent;
+
+public class RecursoCriadoEvent extends ApplicationEvent {
+
+	private static final long serialVersionUID = 4413626517342073574L;
+
+	private HttpServletResponse response;
+	
+	private Long id;
+	
+	public RecursoCriadoEvent(Object source, HttpServletResponse response, Long id) {
+		super(source);
+		this.response = response;
+		this.id = id;
+	}
+
+	public HttpServletResponse getResponse() {
+		return response;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+}
