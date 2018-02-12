4380d05803c004385bb71fc91c5c0f9165e4854c
Matheus Braga <matheusssbraga@gmail.com>
2018-02-12 17:57:12 -0300

    Implementando endpoint para deletar os recursos Pessoa e Categoria

--- src/main/java/com/algaworks/algamoney/api/resource/CategoriaResource.java
+++ src/main/java/com/algaworks/algamoney/api/resource/CategoriaResource.java
@@ -9,11 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algamoney.api.event.RecursoCriadoEvent;
@@ -35,6 +37,11 @@ public class CategoriaResource {
 		return categoriaRepository.findAll();
 	}
 	
+	@GetMapping("/{id}")
+	public Categoria buscarPorID(@PathVariable Long id) {
+		return categoriaRepository.findOne(id);
+	}
+	
 	@PostMapping
 	public ResponseEntity<Categoria> criar(@Valid @RequestBody Categoria categoria, HttpServletResponse response) {
 		categoria = categoriaRepository.save(categoria);
@@ -44,9 +51,10 @@ public class CategoriaResource {
 		return ResponseEntity.status(HttpStatus.CREATED).body(categoria);
 	}
 	
-	@GetMapping("/{id}")
-	public Categoria buscarPorID(@PathVariable Long id) {
-		return categoriaRepository.findOne(id);
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void deletar(@PathVariable Long id) {
+		categoriaRepository.delete(id);
 	}
 	
 }
--- src/main/java/com/algaworks/algamoney/api/resource/PessoaResource.java
+++ src/main/java/com/algaworks/algamoney/api/resource/PessoaResource.java
@@ -9,11 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algamoney.api.event.RecursoCriadoEvent;
@@ -35,6 +37,11 @@ public class PessoaResource {
 		return pessoaRepository.findAll();
 	}
 	
+	@GetMapping("/{id}")
+	public Pessoa buscarPorId(@PathVariable Long id) {
+		return pessoaRepository.findOne(id);
+	}
+	
 	@PostMapping
 	public ResponseEntity<Pessoa> criar(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response) {
 		pessoa = pessoaRepository.save(pessoa);
@@ -44,9 +51,10 @@ public class PessoaResource {
 		return ResponseEntity.status(HttpStatus.CREATED).body(pessoa);
 	}
 	
-	@GetMapping("/{id}")
-	public Pessoa buscarPorId(@PathVariable Long id) {
-		return pessoaRepository.findOne(id);
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void deletar(@PathVariable Long id) {
+		pessoaRepository.delete(id);
 	}
-	
+	 
 }
