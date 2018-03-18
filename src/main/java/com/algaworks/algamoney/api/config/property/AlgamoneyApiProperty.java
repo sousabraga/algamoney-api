f98a1366f47d8f011da6389c39aad0533c82f954
Matheus Braga <matheusssbraga@gmail.com>
2018-03-18 17:36:03 -0300

    Implementando configuração de propriedades de acordo com o profile

---
+++ src/main/java/com/algaworks/algamoney/api/config/property/AlgamoneyApiProperty.java
@@ -0,0 +1,42 @@
+package com.algaworks.algamoney.api.config.property;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("algamoney")
+public class AlgamoneyApiProperty {
+
+	private String origemPermitida = "http://localhost:8000";
+	
+	private Seguranca seguranca = new Seguranca();
+	
+	public String getOrigemPermitida() {
+		return origemPermitida;
+	}
+	
+	public void setOrigemPermitida(String origemPermitida) {
+		this.origemPermitida = origemPermitida;
+	}
+
+	public Seguranca getSeguranca() {
+		return seguranca;
+	}
+
+	public void setSeguranca(Seguranca seguranca) {
+		this.seguranca = seguranca;
+	}
+
+	public static class Seguranca {
+		
+		private boolean enableHttps;
+
+		public boolean isEnableHttps() {
+			return enableHttps;
+		}
+
+		public void setEnableHttps(boolean enableHttps) {
+			this.enableHttps = enableHttps;
+		}
+
+	}
+	
+}
--- src/main/java/com/algaworks/algamoney/api/cors/CorsFilter.java
+++ src/main/java/com/algaworks/algamoney/api/cors/CorsFilter.java
@@ -11,15 +11,19 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+
+import com.algaworks.algamoney.api.config.property.AlgamoneyApiProperty;
 
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsFilter implements Filter {
 
-	private String origemPermitida = "http://localhost:8000"; // TODO Configurar para diferentes ambientes
+	@Autowired
+	private AlgamoneyApiProperty algamoneyApiProperty;
 	
 	@Override
 	public void init(FilterConfig config) throws ServletException {}
@@ -30,10 +34,10 @@ public class CorsFilter implements Filter {
 		HttpServletRequest req = (HttpServletRequest) request;
 		HttpServletResponse res = (HttpServletResponse) response;
 		
-		res.setHeader("Access-Control-Allow-Origin", origemPermitida);
+		res.setHeader("Access-Control-Allow-Origin", algamoneyApiProperty.getOrigemPermitida());
 		res.setHeader("Access-Control-Allow-Credentials", "true");
 		
-		if ("OPTIONS".equals(req.getMethod()) && origemPermitida.equals(res.getHeader("Origin"))) {
+		if ("OPTIONS".equals(req.getMethod()) && algamoneyApiProperty.getOrigemPermitida().equals(res.getHeader("Origin"))) {
 			res.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, OPTIONS");
 			res.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept");
 			res.setHeader("Access-Control-Max-Age", "3600");
