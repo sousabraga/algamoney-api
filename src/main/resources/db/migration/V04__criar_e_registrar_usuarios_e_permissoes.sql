0bf01bbb2b5e23225a1a0398bd08c233a2ef2dfa
Matheus Braga <matheusssbraga@gmail.com>
2018-11-13 21:39:22 -0300

    Ajustando script de permissões

--- src/main/resources/db/migration/V04__criar_e_registrar_usuarios_e_permissoes.sql
+++ src/main/resources/db/migration/V04__criar_e_registrar_usuarios_e_permissoes.sql
@@ -22,25 +22,32 @@ INSERT INTO usuario (id, nome, email, senha) values (1, 'Administrador', 'admin@
 INSERT INTO usuario (id, nome, email, senha) values (2, 'Maria Silva', 'maria@algamoney.com', '$2a$10$Zc3w6HyuPOPXamaMhh.PQOXvDnEsadztbfi6/RyZWJDzimE8WQjaq');
 
 INSERT INTO permissao (id, descricao) values (1, 'ROLE_CADASTRAR_CATEGORIA');
-INSERT INTO permissao (id, descricao) values (2, 'ROLE_PESQUISAR_CATEGORIA');
+INSERT INTO permissao (id, descricao) values (2, 'ROLE_REMOVER_CATEGORIA');
+INSERT INTO permissao (id, descricao) values (3, 'ROLE_PESQUISAR_CATEGORIA');
+INSERT INTO permissao (id, descricao) values (4, 'ROLE_ATUALIZAR_CATEGORIA');
 
-INSERT INTO permissao (id, descricao) values (3, 'ROLE_CADASTRAR_PESSOA');
-INSERT INTO permissao (id, descricao) values (4, 'ROLE_REMOVER_PESSOA');
-INSERT INTO permissao (id, descricao) values (5, 'ROLE_PESQUISAR_PESSOA');
+INSERT INTO permissao (id, descricao) values (5, 'ROLE_CADASTRAR_PESSOA');
+INSERT INTO permissao (id, descricao) values (6, 'ROLE_REMOVER_PESSOA');
+INSERT INTO permissao (id, descricao) values (7, 'ROLE_PESQUISAR_PESSOA');
+INSERT INTO permissao (id, descricao) values (8, 'ROLE_ATUALIZAR_PESSOA');
 
-INSERT INTO permissao (id, descricao) values (6, 'ROLE_CADASTRAR_LANCAMENTO');
-INSERT INTO permissao (id, descricao) values (7, 'ROLE_REMOVER_LANCAMENTO');
-INSERT INTO permissao (id, descricao) values (8, 'ROLE_PESQUISAR_LANCAMENTO');
+INSERT INTO permissao (id, descricao) values (9, 'ROLE_CADASTRAR_LANCAMENTO');
+INSERT INTO permissao (id, descricao) values (10, 'ROLE_REMOVER_LANCAMENTO');
+INSERT INTO permissao (id, descricao) values (11, 'ROLE_PESQUISAR_LANCAMENTO');
+INSERT INTO permissao (id, descricao) values (12, 'ROLE_ATUALIZAR_LANCAMENTO');
 
 -- admin
 INSERT INTO usuario_permissao (id_usuario, id_permissao) values (1, 1);
 INSERT INTO usuario_permissao (id_usuario, id_permissao) values (1, 2);
 INSERT INTO usuario_permissao (id_usuario, id_permissao) values (1, 3);
-INSERT INTO usuario_permissao (id_usuario, id_permissao) values (1, 4);
 INSERT INTO usuario_permissao (id_usuario, id_permissao) values (1, 5);
 INSERT INTO usuario_permissao (id_usuario, id_permissao) values (1, 6);
 INSERT INTO usuario_permissao (id_usuario, id_permissao) values (1, 7);
 INSERT INTO usuario_permissao (id_usuario, id_permissao) values (1, 8);
+INSERT INTO usuario_permissao (id_usuario, id_permissao) values (1, 9);
+INSERT INTO usuario_permissao (id_usuario, id_permissao) values (1, 10);
+INSERT INTO usuario_permissao (id_usuario, id_permissao) values (1, 11);
+INSERT INTO usuario_permissao (id_usuario, id_permissao) values (1, 12);
 
 -- maria
 INSERT INTO usuario_permissao (id_usuario, id_permissao) values (2, 2);
